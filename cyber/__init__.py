e8722e546e108786441fa95302d8612350e9a3da
Theekshana Jayasinghe <73696370+DaVe-Smith-89@users.noreply.github.com>
2026-08-29 14:07:15 +0530

    Clear sensitive configuration values in init.py
    Removed sensitive configuration values and set them to empty strings.

--- cyber/__init__.py
+++ cyber/__init__.py
@@ -13,25 +13,25 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 admin = Admin(app, url='/3xf8z81HUddaTkyqZBXJm9PG', template_mode='bootstrap4')
 
-app.config['SECRET_KEY'] = 'e958818ff079dd18c81aca2005bdf485'
+app.config['SECRET_KEY'] = ''
 app.config['MAIL_SERVER'] = 'smtp.zoho.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'services@slcyberwarriors.com'
-app.config['MAIL_PASSWORD'] = 'Shiba/24289bruno?**'
-app.config['GOOGLE_CLIENT_ID'] = '1020596954536-rg1a2ks6bjai7pa1pba4bmgqfc3rptsq.apps.googleusercontent.com'
-app.config['GOOGLE_CLIENT_SECRET'] = '3xf8z81HUddaTkyqZBXJm9PG'
-app.config['GITHUB_CLIENT_ID'] = 'dcebb3bc56936fe47c62'
-app.config['GITHUB_CLIENT_SECRET'] = '8ec4e88f6eb22dd989f171ce7b2e79fa0e72886f'
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LezkmYcAAAAAOLp99vfkPuxNSDKI6dEX5yE5esz'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LezkmYcAAAAADCSPzTnWA_j_32h_Pv0V7JjuONT'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ubxl94e8eqocqnbzrvq0:u5UDXo7M44kofPpVH3Ix@bqve9ipvrxcwc5nlzlad-postgresql.services.clever-cloud.com:5432/bqve9ipvrxcwc5nlzlad'
-# app.config['SQLALCHEMY_BINDS'] = {'second': 'postgresql://unwgacywkotr1fqk1jeq:gtKETs6nmvrmC7fPo5Wo@borbeftk5rwwoj24bubc-postgresql.services.clever-cloud.com:5432/borbeftk5rwwoj24bubc'}
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cvhfpylqxiwwpp:6f8d6d887a1d8e9c56c5a10191dc41aef491356846b2f289ed26795a011121fb@ec2-3-221-100-217.compute-1.amazonaws.com:5432/dab051kol0kknc'
-app.config['SQLALCHEMY_BINDS'] = {'second': 'postgresql://pcaixxtummdant:ccc4eb6b2090b6c4fd25be4555835654a84b9693dfc2eeb803f4f04398aeabf9@ec2-3-221-100-217.compute-1.amazonaws.com:5432/d29ludnn58sdfv'}
+app.config['MAIL_USERNAME'] = '
+app.config['MAIL_PASSWORD'] = ''
+app.config['GOOGLE_CLIENT_ID'] = ''
+app.config['GOOGLE_CLIENT_SECRET'] = ''
+app.config['GITHUB_CLIENT_ID'] = ''
+app.config['GITHUB_CLIENT_SECRET'] = ''
+app.config['RECAPTCHA_PUBLIC_KEY'] = ''
+app.config['RECAPTCHA_PRIVATE_KEY'] = ''
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# app.config['SQLALCHEMY_BINDS'] = {'second': ''}
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_BINDS'] = {'second': ''}
 
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = ''
-admins = ['theekshanapramod2580@gmail.com', 'johnkener118@gmail.com']
+admins = ['', '']
 
 google = oauth.register(
     name = 'google',
